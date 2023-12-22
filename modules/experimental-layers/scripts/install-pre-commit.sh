491606505916c6f9c471909cc8f07eb2dd225f95
Ib Green <ib@unfolded.ai>
2023-12-22 09:09:32 -0500

    chore(layers): Consolidate layers module (#30)

--- modules/experimental-layers/scripts/install-pre-commit.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cp 'scripts/pre-commit' '.git/hooks/pre-commit'
-echo "Installed!"
--- modules/experimental-layers/src/index.ts
+++
@@ -1,9 +0,0 @@
-//
-// SPDX-License-Identifier: MIT
-// Copyright (c) vis.gl contributors
-
-export {TileSourceLayer} from './tile-source-layer';
-export {DataDrivenTile3DLayer} from './data-driven-tile-3d-layer/data-driven-tile-3d-layer';
-
-export {colorizeTile} from './data-driven-tile-3d-layer/utils/colorize-tile';
-export {filterTile} from './data-driven-tile-3d-layer/utils/filter-tile';
