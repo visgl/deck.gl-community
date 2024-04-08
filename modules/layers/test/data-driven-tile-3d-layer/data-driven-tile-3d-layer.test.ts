33facc1070c82c7938077726aa260ad8e9f5dc70
Don McCurdy <donmccurdy@cartodb.com>
2024-04-08 16:01:38 -0400

    split tests, one test file per source file

---
+++ modules/layers/test/data-driven-tile-3d-layer/data-driven-tile-3d-layer.test.ts
@@ -0,0 +1,8 @@
+import {describe, it, expect} from 'vitest';
+import {DataDrivenTile3DLayer} from '../../src';
+
+describe('DataDrivenTile3DLayer', () => {
+  it('exports', () => {
+    expect(DataDrivenTile3DLayer).toBeTruthy();
+  });
+});
---
+++ modules/layers/test/data-driven-tile-3d-layer/utils/filter-tile.test.ts
@@ -0,0 +1,8 @@
+import {describe, it, expect} from 'vitest';
+import {filterTile} from '../../../src';
+
+describe('filterTile', () => {
+  it('exports', () => {
+    expect(filterTile).toBeTruthy();
+  });
+});
