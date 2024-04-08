1cacc1bc966e0f1d1aace45b024cd8db37d83170
Don McCurdy <donmccurdy@cartodb.com>
2024-04-08 16:07:11 -0400

    chore(layers): Add basic tests (#46)

---
+++ modules/layers/test/data-driven-tile-3d-layer/utils/colorize-tile.test.ts
@@ -0,0 +1,8 @@
+import {describe, it, expect} from 'vitest';
+import {colorizeTile} from '../../../src';
+
+describe('colorizeTile', () => {
+  it('exports', () => {
+    expect(colorizeTile).toBeTruthy();
+  });
+});
---
+++ modules/layers/test/tile-source-layer.test.ts
@@ -0,0 +1,8 @@
+import {describe, it, expect} from 'vitest';
+import {TileSourceLayer} from '../src';
+
+describe('TileSourceLayer', () => {
+  it('exports', () => {
+    expect(TileSourceLayer).toBeTruthy();
+  });
+});
