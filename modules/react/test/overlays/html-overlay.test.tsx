76062f61a29eb8b5d5524c3908e5d4424b628ee4
Ib Green <ib@foursquare.com>
2024-04-11 17:23:30 -0400

    wio

---
+++ modules/react/test/overlays/html-overlay.test.tsx
@@ -0,0 +1,68 @@
+import {it, expect} from 'vitest';
+import React from 'react';
+/*
+import Renderer from 'react-test-renderer';
+
+import DeckGL from '@deck.gl/react';
+import { createHeadlessContext } from '@luma.gl/test-utils';
+*/
+import HtmlOverlay from '../../src/overlays/html-overlay';
+import HtmlOverlayItem from '../../src/overlays/html-overlay-item';
+
+/*
+const initialViewState = {
+  longitude: 0,
+  latitude: 0,
+  zoom: 10,
+  pitch: 0,
+  bearing: 0,
+};
+
+it('test HtmlOverlay map center', () => {
+  const Component = (
+    // @ts-ignore
+    <DeckGL
+      gl={createHeadlessContext({ width: 100, height: 100, webgl1: true })}
+      initialViewState={initialViewState}
+    >
+      <HtmlOverlay>
+        <HtmlOverlayItem coordinates={[0, 0, 0]}>Map Center Zero Elevation</HtmlOverlayItem>
+        <HtmlOverlayItem coordinates={[0, 0, 50000]}>Map Center 50km Elevation</HtmlOverlayItem>
+      </HtmlOverlay>
+    </DeckGL>
+  );
+  const renderer = Renderer.create(Component);
+  expect(renderer.toJSON()).toMatchSnapshot();
+});
+*/
+
+it('HtmlOverlay is able to handle a single null child', () => {
+  const layer = new HtmlOverlay({
+    viewport: { project: (coords) => coords },
+  });
+
+  expect(layer.render()).toMatchSnapshot();
+});
+
+it('HtmlOverlay is able to handle a null child with other valid children', () => {
+  const children = [
+    <HtmlOverlayItem key={1} coordinates={[0, 0, 0]}>
+      Map Center Zero Elevation
+    </HtmlOverlayItem>,
+    <HtmlOverlayItem key={2} coordinates={[0, 0, 50000]}>
+      Map Center 50km Elevation
+    </HtmlOverlayItem>,
+    null,
+  ];
+
+  const layer = new HtmlOverlay({
+    children,
+    viewport: { project: (coords) => coords },
+  });
+  expect(layer.render()).toMatchSnapshot();
+});
+
+it('HtmlOverlay is able to handle no children', () => {
+  const layer = new HtmlOverlay({ viewport: { project: (coords) => coords } });
+  expect(layer.render()).toMatchSnapshot();
+});
