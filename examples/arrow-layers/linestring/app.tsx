f2cd117b3584e2cb236dbac32684e7d39665c360
Ib Green <ib@unfolded.ai>
2024-04-18 16:54:07 -0400

    feat(arrow-layers): Fork @geoarrow/deck.gl-layers (#67)

---
+++ examples/arrow-layers/linestring/app.tsx
@@ -0,0 +1,80 @@
+import React, {useState, useEffect} from 'react';
+import {createRoot} from 'react-dom/client';
+import {StaticMap, MapContext, NavigationControl} from 'react-map-gl';
+import DeckGL, {Layer, PickingInfo} from 'deck.gl';
+import {GeoArrowPathLayer} from '@deck.gl-community/arrow-layers';
+import * as arrow from 'apache-arrow';
+
+const GEOARROW_POINT_DATA = 'http://localhost:8080/ne_10m_roads_north_america.arrow';
+
+const INITIAL_VIEW_STATE = {
+  latitude: 40,
+  longitude: -95,
+  zoom: 4,
+  bearing: 0,
+  pitch: 0
+};
+
+const MAP_STYLE = 'https://basemaps.cartocdn.com/gl/positron-nolabels-gl-style/style.json';
+const NAV_CONTROL_STYLE = {
+  position: 'absolute',
+  top: 10,
+  left: 10
+};
+
+function Root() {
+  const onClick = (info: PickingInfo) => {
+    if (info.object) {
+      console.log(JSON.stringify(info.object.toJSON()));
+    }
+  };
+
+  const [table, setTable] = useState<arrow.Table | null>(null);
+
+  useEffect(() => {
+    // declare the data fetching function
+    const fetchData = async () => {
+      const data = await fetch(GEOARROW_POINT_DATA);
+      const buffer = await data.arrayBuffer();
+      const table = arrow.tableFromIPC(buffer);
+      setTable(table);
+    };
+
+    if (!table) {
+      fetchData().catch(console.error);
+    }
+  });
+
+  const layers: Layer[] = [];
+
+  table &&
+    layers.push(
+      new GeoArrowPathLayer({
+        id: 'geoarrow-linestring',
+        data: table,
+        getColor: [255, 0, 0],
+        widthMinPixels: 1,
+        pickable: true
+      })
+    );
+
+  return (
+    <DeckGL
+      initialViewState={INITIAL_VIEW_STATE}
+      controller={true}
+      layers={layers}
+      ContextProvider={MapContext.Provider}
+      onClick={onClick}
+      glOptions={{
+        powerPreference: 'high-performance'
+      }}
+    >
+      <StaticMap mapStyle={MAP_STYLE} />
+      <NavigationControl style={NAV_CONTROL_STYLE} />
+    </DeckGL>
+  );
+}
+
+/* global document */
+const container = document.body.appendChild(document.createElement('div'));
+createRoot(container).render(<Root />);
---
+++ modules/arrow-layers/src/index.ts
@@ -0,0 +1,25 @@
+export {GeoArrowArcLayer} from './arc-layer';
+export {GeoArrowColumnLayer} from './column-layer';
+export {GeoArrowH3HexagonLayer as _GeoArrowH3HexagonLayer} from './h3-hexagon-layer';
+export {GeoArrowHeatmapLayer} from './heatmap-layer';
+export {GeoArrowPathLayer} from './path-layer';
+export {GeoArrowPointCloudLayer} from './point-cloud-layer';
+export {GeoArrowPolygonLayer} from './polygon-layer';
+export {GeoArrowScatterplotLayer} from './scatterplot-layer';
+export {GeoArrowSolidPolygonLayer} from './solid-polygon-layer';
+export {GeoArrowTextLayer as _GeoArrowTextLayer} from './text-layer';
+export {GeoArrowTripsLayer} from './trips-layer';
+
+export type {GeoArrowArcLayerProps} from './arc-layer';
+export type {GeoArrowColumnLayerProps} from './column-layer';
+export type {GeoArrowH3HexagonLayerProps as _GeoArrowH3HexagonLayerProps} from './h3-hexagon-layer';
+export type {GeoArrowHeatmapLayerProps} from './heatmap-layer';
+export type {GeoArrowPathLayerProps} from './path-layer';
+export type {GeoArrowPointCloudLayerProps} from './point-cloud-layer';
+export type {GeoArrowPolygonLayerProps} from './polygon-layer';
+export type {GeoArrowScatterplotLayerProps} from './scatterplot-layer';
+export type {GeoArrowSolidPolygonLayerProps} from './solid-polygon-layer';
+export type {GeoArrowTextLayerProps as _GeoArrowTextLayerProps} from './text-layer';
+export type {GeoArrowTripsLayerProps} from './trips-layer';
+
+export type {GeoArrowPickingInfo} from './types';
