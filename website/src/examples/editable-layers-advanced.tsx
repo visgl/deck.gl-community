67b51dfff7d8ec474d4dd7bc890fcab2fdc1d913
Birk Skyum <birk.skyum@pm.me>
2024-05-02 21:38:32 +0200

    convert examples to typescript

---
+++ website/src/examples/editable-layers-advanced.tsx
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import {GITHUB_TREE} from '../constants/defaults';
+import App from '../../../examples/editable-layers/advanced/src/example';
+
+import {makeExample} from '../components';
+
+class AdvancedDemo extends Component {
+  static title = 'Advanced';
+
+  static code = `${GITHUB_TREE}/examples/editable-layers/advanced`;
+
+  static renderInfo(meta) {
+    return (
+      <>
+      </>
+    );
+  }
+
+  render() {
+    const {...otherProps} = this.props;
+
+    return (
+      <App
+        {...otherProps}
+      />
+    );
+  }
+}
+
+export default makeExample(AdvancedDemo);
---
+++ website/src/examples/editable-layers-editor.tsx
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import {GITHUB_TREE} from '../constants/defaults';
+import {Example as App} from '../../../examples/editable-layers/editor/example';
+
+import {makeExample} from '../components';
+
+class EditorDemo extends Component {
+  static title = 'Editor';
+
+  static code = `${GITHUB_TREE}/examples/editable-layers/editor`;
+
+  static renderInfo(meta) {
+    return (
+      <>
+      </>
+    );
+  }
+
+  render() {
+    const {...otherProps} = this.props;
+
+    return (
+      <App
+        {...otherProps}
+      />
+    );
+  }
+}
+
+export default makeExample(EditorDemo);
