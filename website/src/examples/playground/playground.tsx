369c24858a481c4fcbca8611592a0a4bd2bc9d3d
Ib Green <ib@foursquare.com>
2024-11-20 16:03:15 -0500

    fixes

--- website/src/examples/playground/playground.tsx
+++ website/src/examples/playground/playground.tsx
@@ -20,7 +20,21 @@ class AdvancedDemo extends Component {
 
     return (
       <BrowserOnly>
-        {() => <App {...otherProps} />}
+        {() => (
+          <>
+            <style>
+              {`
+    body {margin: 0; font-family: sans-serif; overflow: hidden;}
+    #app {width: 100vw; height: 100vh; display: flex; flex-direction: row; align-items: stretch;}
+    #left-pane {flex: 0 1 40%; display: flex; flex-direction: column; align-items: stretch;}
+    #left-pane select {flex: 0 0 34px; padding: 5px 35px 5px 5px; font-size: 16px; border: 1px solid #ccc; appearance: none;}
+    #editor {flex: 0 1 100%;}
+    #right-pane {flex: 0 1 60%; position: relative;}
+    `}
+            </style>
+            <App {...otherProps} />
+          </>
+        )}
       </BrowserOnly>
     );
   }
