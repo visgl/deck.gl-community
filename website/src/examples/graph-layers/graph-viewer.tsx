f8b0170f2451654d965b30e6cccf8a668459b0f9
Ib Green <ib@foursquare.com>
2024-11-20 09:29:51 -0500

    chore(graph-layers): Add example to website (#158)

---
+++ website/src/examples/graph-layers/graph-viewer.tsx
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import BrowserOnly from '@docusaurus/BrowserOnly';
+
+import {GITHUB_TREE} from '../../constants/defaults';
+import {App} from '../../../../examples/graph-layers/graph-viewer/app';
+
+import {makeExample} from '../../components';
+
+class AdvancedDemo extends Component {
+  static title = 'Simple Graph Viewer';
+
+  static code = `${GITHUB_TREE}/examples/graph-layers/graph-viewer`;
+
+  static renderInfo(meta) {
+    return <></>;
+  }
+
+  render() {
+    const {...otherProps} = this.props;
+
+    return (
+      <BrowserOnly>
+        {() => <App {...otherProps} />}
+      </BrowserOnly>
+    );
+  }
+}
+
+export default makeExample(AdvancedDemo);
