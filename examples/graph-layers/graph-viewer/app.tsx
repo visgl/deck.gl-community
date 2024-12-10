7b82cf895c5e2cffe6f4342d301271847d3b2382
Ib Green <ib@unfolded.ai>
2024-12-10 13:32:49 -0500

    wip

--- examples/graph-layers/graph-viewer/app.tsx
+++ examples/graph-layers/graph-viewer/app.tsx
@@ -5,20 +5,45 @@
 import React, {Component, useCallback, useEffect, useLayoutEffect, useMemo, useState, useReducer, useRef} from 'react';
 import {createRoot} from 'react-dom/client';
 
-import {D3ForceLayout, JSONLoader, NODE_TYPE} from '@deck.gl-community/graph-layers';
+import DeckGL from '@deck.gl/react';
+import {PositionedViewControl} from '@deck.gl-community/react';
+
+import {OrthographicView} from '@deck.gl/core';
+import {GraphEngine, GraphLayer, Graph, log, BaseLayout, SimpleLayout, D3ForceLayout, JSONLoader, NODE_TYPE} from '@deck.gl-community/graph-layers';
+
+// import {ViewControlWidget} from '@deck.gl-community/graph-layers';
+import '@deck.gl/widgets/stylesheet.css';
+
+import {extent} from 'd3-array';
 
 // eslint-disable-next-line import/no-unresolved
 import {SAMPLE_GRAPH_DATASETS} from '../../../modules/graph-layers/test/data/graphs/sample-datasets';
 
-import PropTypes from 'prop-types';
-import DeckGL from '@deck.gl/react';
-import {OrthographicView} from '@deck.gl/core';
-import {GraphEngine, BaseLayout, Graph, GraphLayer, log, SimpleLayout} from '@deck.gl-community/graph-layers';
-import {PositionedViewControl} from '@deck.gl-community/react';
-import {ViewControlWidget} from '@deck.gl-community/graph-layers';
-import '@deck.gl/widgets/stylesheet.css';
+const INITIAL_VIEW_STATE = {
+  /** the target origin of the view */
+  target: [0, 0],
+  /** zoom level */
+  zoom: 1
+};
 
-import {extent} from 'd3-array';
+const LAYOUTS = ['D3ForceLayout', 'GPUForceLayout', 'SimpleLayout'];
+
+// the default cursor in the view
+const DEFAULT_CURSOR = 'default';
+const DEFAULT_NODE_SIZE = 5;
+const DEFAULT_DATASET = 'Random (20, 40)';
+
+// const nodeEvents = {
+//   onMouseEnter: null,
+//   onHover: null,
+//   onMouseLeave: null,
+//   onClick: null,
+//   onDrag: null
+// },
+//   edgeEvents = {
+//   onClick: null,
+//   onHover: null
+// },
 
 
 const loadingReducer = (state, action) => {
@@ -60,76 +85,6 @@ export const useLoading = (engine) => {
   return [{isLoading}, loadingDispatch];
 };
 
-const INITIAL_VIEW_STATE = {
-  // the target origin of th view
-  target: [0, 0],
-  // zoom level
-  zoom: 1
-};
-
-// the default cursor in the view
-const DEFAULT_CURSOR = 'default';
-
-// GraphGL.propTypes = {
-//   /** Input graph data */
-//   graph: PropTypes.object.isRequired,
-//   /** Layout algorithm */
-//   layout: PropTypes.object.isRequired,
-//   /** Node event callbacks */
-//   nodeEvents: PropTypes.shape({
-//     onClick: PropTypes.func,
-//     onMouseLeave: PropTypes.func,
-//     onHover: PropTypes.func,
-//     onMouseEnter: PropTypes.func
-//   }).isRequired,
-//   /** Declarative node style */
-//   nodeStyle: PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.object, PropTypes.bool])),
-//   /** Declarative edge style */
-//   edgeStyle: PropTypes.oneOfType([
-//     PropTypes.object,
-//     PropTypes.arrayOf(
-//       PropTypes.shape({
-//         stroke: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
-//         strokeWidth: PropTypes.oneOfType([PropTypes.number, PropTypes.func]),
-//         decorators: PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.object, PropTypes.bool]))
-//       })
-//     )
-//   ]).isRequired,
-//   /** Edge event callbacks */
-//   edgeEvents: PropTypes.shape({
-//     onClick: PropTypes.func,
-//     onHover: PropTypes.func
-//   }),
-//   /** Error callback */
-//   onError: PropTypes.func,
-//   /** The initial view state of the viewport */
-//   initialViewState: PropTypes.shape({
-//     target: PropTypes.arrayOf(PropTypes.number),
-//     zoom: PropTypes.number
-//   }),
-//   /** A minimum scale factor for zoom level of the graph. */
-//   minZoom: PropTypes.number,
-//   /** A maximum scale factor for zoom level of the graph. */
-//   maxZoom: PropTypes.number,
-//   /** Padding for fitting entire graph in the screen. (pixel) */
-//   viewportPadding: PropTypes.number,
-//   /** Changes the scroll wheel sensitivity when zooming. This is a multiplicative modifier.
-//    So, a value between 0 and 1 reduces the sensitivity (zooms slower),
-//    and a value greater than 1 increases the sensitivity (zooms faster) */
-//   wheelSensitivity: PropTypes.number,
-//   /** Whether zooming the graph is enabled */
-//   /** Whether dragging the node is enabled */
-//   enableDragging: PropTypes.bool,
-//   /** Resume layout calculation after dragging a node */
-//   resumeLayoutAfterDragging: PropTypes.bool,
-// };
-
-const DEFAULT_NODE_SIZE = 5;
-
-const DEFAULT_DATASET = 'Random (20, 40)';
-
-const LAYOUTS = ['D3ForceLayout', 'GPUForceLayout', 'SimpleLayout'];
-
 const graphData = SAMPLE_GRAPH_DATASETS[DEFAULT_DATASET]();
 const graph = JSONLoader({json: graphData});
 const layout = new D3ForceLayout(); // SimpleLayout();
@@ -164,39 +119,21 @@ export function App(props) {
     };
   }, [engine]);
 
-  const nodeStyle = [],
-    nodeEvents = {
-      onMouseEnter: null,
-      onHover: null,
-      onMouseLeave: null,
-      onClick: null,
-      onDrag: null
-    },
-    edgeStyle = [
-      {
-        decorators: [],
-        stroke: 'black',
-        strokeWidth: 1
-      }
-    ],
-    edgeEvents = {
-      onClick: null,
-      onHover: null
-    },
+  const edgeStyle = [
+    {
+      decorators: [],
+      stroke: 'black',
+      strokeWidth: 1
+    }
+  ],
     // eslint-disable-next-line no-console
-    onError = (error) => console.error(error),
     initialViewState = INITIAL_VIEW_STATE,
     minZoom = -20,
     maxZoom = 20,
     viewportPadding = 50,
-    wheelSensitivity = 0.5,
     enableDragging = false,
     resumeLayoutAfterDragging = false,
     zoomToFitOnLoad = false;
-
-  let loader = null,
-    getTooltip,
-    onHover;
 
   const [viewState, setViewState] = useState({
     ...INITIAL_VIEW_STATE,
@@ -263,57 +200,47 @@ export function App(props) {
     };
   }, [engine, isLoading, fitBounds, zoomToFitOnLoad]);
 
-  // const handleChangeGraph = ({target: {value}}) => setState(state => ({...state, selectedDataset: value}));
-  // const handleChangeLayout = ({target: {value}}) => setState(state => ({...state, selectedLayout: value}));
 
-  // return (
-  //    <div style={{width: '100%', zIndex: 999}}>
-  //       <div>
-  //         Dataset:
-  //         <select value={state.selectedDataset} onChange={this.handleChangeGraph}>
-  //           {Object.keys(SAMPLE_GRAPH_DATASETS).map((data) => (
-  //             <option key={data} value={data}>
-  //               {data}
-  //             </option>
-  //           ))}
-  //         </select>
-  //       </div>
-  //       <div>
-  //         Layout:
-  //         <select value={state.selectedLayout} onChange={this.handleChangeLayout}>
-  //           {LAYOUTS.map((data) => (
-  //             <option key={data} value={data}>
-  //               {data}
-  //             </option>
-  //           ))}
-  //         </select>
-  //       </div>
-  //     </div>
+  const handleChangeGraph = useCallback(({target: {value}}) => setState(state => ({...state, selectedDataset: value})), [setState]);
+  const handleChangeLayout = useCallback(({target: {value}}) => setState(state => ({...state, selectedLayout: value})), [setState]);
 
   return (
     <div style={{display: 'flex', flexDirection: 'column', height: '100%'}}>
+      <div style={{width: '100%', zIndex: 999}}>
+        <div>
+          Dataset:
+          <select value={state.selectedDataset} onChange={handleChangeGraph}>
+            {Object.keys(SAMPLE_GRAPH_DATASETS).map((data) => (
+              <option key={data} value={data}>
+                {data}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
+          Layout:
+          <select value={state.selectedLayout} onChange={handleChangeLayout}>
+            {LAYOUTS.map((data) => (
+              <option key={data} value={data}>
+                {data}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
       <div style={{width: '100%', flex: 1}}>
         <>
-          {isLoading && loader}
+          {isLoading}
           <div style={{visibility: isLoading ? 'hidden' : 'visible'}}>
             <DeckGL
-              onError={onError}
-              onAfterRender={useCallback(
-                () => loadingDispatch({type: 'afterRender'}),
-                [loadingDispatch]
-              )}
+              onError={(error) => console.error(error)}
+              onAfterRender={() => loadingDispatch({type: 'afterRender'})}
               width="100%"
               height="100%"
-              getCursor={useCallback(() => DEFAULT_CURSOR, [])}
+              getCursor={() => DEFAULT_CURSOR}
               viewState={viewState as any}
-              onResize={useCallback(
-                ({width, height}) => setViewState((prev) => ({...prev, width, height})),
-                []
-              )}
-              onViewStateChange={useCallback(
-                ({viewState: nextViewState}) => setViewState(nextViewState as any),
-                []
-              )}
+              onResize={({width, height}) => setViewState((prev) => ({...prev, width, height}))}
+              onViewStateChange={({viewState}) => setViewState(viewState as any)}
               views={[
                 new OrthographicView({
                   controller: {
@@ -322,7 +249,8 @@ export function App(props) {
                     scrollZoom: true,
                     touchZoom: true,
                     doubleClickZoom: true,
-                    dragPan: true
+                    dragPan: true,
+                    wheelSensitivity: 0.5
                   } as any
                 })
               ]}
@@ -337,23 +265,21 @@ export function App(props) {
                     }
                   ],
                   edgeStyle: {
-                    stroke: '#000',
+                    decorators: [],
+                    stroke: 'black',
                     strokeWidth: 1
                   },
-                  nodeEvents,
-                  edgeEvents,
-                  enableDragging,
                   resumeLayoutAfterDragging
                 })
               ]}
               widgets={[
-                // TODO - replace 
-                // new ViewControlWidget({})
-              ]}
-              getTooltip={getTooltip}
-              onHover={onHover}
+                // // new ViewControlWidget({}) TODO - fix and enable
+              ]
+                // onHover={(info) => console.log('Hover', info)}
+              }
+              getTooltip={(info) => getToolTip(info.object)}
             />
-            {/* View control component */
+            {/* View control component TODO - replace with widget */
               <PositionedViewControl
                 fitBounds={fitBounds}
                 panBy={panBy}
@@ -368,6 +294,14 @@ export function App(props) {
       </div>
     </div>
   );
+}
+
+function getToolTip(object) {
+  if (!object) {
+    return null;
+  }
+  const type = object.isNode ? 'Node' : 'Edge';
+  return `${type}: ${JSON.stringify(object?._data)}`;
 }
 
 export function renderToDOM() {
--- modules/react/src/components/positioned-view-control.tsx
+++ modules/react/src/components/positioned-view-control.tsx
@@ -5,8 +5,10 @@
 import {ViewControl} from './view-control';
 import React from 'react';
 
+
+
 // A wrapper for positioning the ViewControl component
-export const PositionedViewControl = ({fitBounds, panBy, zoomBy, zoomLevel, maxZoom, minZoom}) => (
+export const PositionedViewControl = ({fitBounds, panBy, zoomBy, zoomLevel, maxZoom = 20, minZoom = -20}) => (
   <div style={{position: 'relative', top: '20px', left: '20px'}}>
     <ViewControl
       fitBounds={fitBounds}
