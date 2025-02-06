e0ce4730c1d2c921f122fd5bb79b2979a848a7a1
charlieforward9 <charlesrichardsonusa@gmail.com>
2025-02-06 11:35:46 -0500

    push latest from #201 to example

--- examples/editable-layers/overlaid/example.tsx
+++ examples/editable-layers/overlaid/example.tsx
@@ -1,21 +1,52 @@
-import React from 'react';
+import React, {forwardRef, useImperativeHandle} from 'react';
 import {Map, useControl} from 'react-map-gl/maplibre';
-import {DeckProps} from 'deck.gl';
-import {MapboxOverlay} from '@deck.gl/mapbox';
+import {MapController} from 'deck.gl';
+import {MapboxOverlay, type MapboxOverlayProps} from '@deck.gl/mapbox';
 import {
   DrawLineStringMode,
   DrawPolygonMode,
-  EditableGeoJsonLayer
+  EditableGeoJsonLayer,
+  type FeatureCollection
 } from '@deck.gl-community/editable-layers';
+import type {MjolnirEvent} from 'mjolnir.js';
 
-function DeckGLOverlay(props: DeckProps) {
-  const overlay = useControl<MapboxOverlay>(() => new MapboxOverlay(props));
-  overlay.setProps(props);
-  return null;
+export class EditableLayerMapController extends MapController {
+  constructor(props) {
+    super(props);
+    // We cannot enable 'anyclick', because this seems to be not known to mjolnir.js anymore.
+    this.events = ['click'];
+  }
+  handleEvent(event: MjolnirEvent): boolean {
+    if (event.type === 'click') {
+      // @ts-ignore https://github.com/visgl/deck.gl-community/issues/201
+      this.eventManager.manager.emit('anyclick', event);
+    }
+    return super.handleEvent(event);
+  }
 }
 
+export const DeckGLOverlay = forwardRef(function DeckGLOverlay(
+  props: MapboxOverlayProps & {
+    interleaved?: boolean;
+  },
+  ref
+) {
+  const overlay = useControl(() => new MapboxOverlay(props), {});
+  overlay.setProps({
+    ...props,
+    interleaved: props.interleaved ?? false,
+    /** @ts-expect-error: DeckGL expects a controller prop, but it's not in the MapboxOverlayProps type */
+    controller: {
+      type: EditableLayerMapController
+    }
+  });
+
+  useImperativeHandle(ref, () => overlay, [overlay]);
+  return null;
+});
+
 export default function OverlaidEditable() {
-  const [features, setFeatures] = React.useState({
+  const [features, setFeatures] = React.useState<FeatureCollection>({
     type: 'FeatureCollection',
     features: []
   });
@@ -26,21 +57,19 @@ export default function OverlaidEditable() {
     data: features,
     mode,
     selectedFeatureIndexes,
+    pickable: true,
+    editHandleType: 'point', // Ensure points appear when editing
     onEdit: ({updatedData}) => {
-      //FIXME: this event is not fired...
+      // FIXME: this event is not fired...
       console.log('updatedData', updatedData);
       setFeatures(updatedData);
     }
   });
 
-  React.useEffect(() => {
-    console.log(`mode changed to ${mode.name}`);
-  }, [mode]);
-
   return (
     <div>
       <Map
-        style={{width: '100%', height: 500}} //FIXME: unable to get height 100% to work
+        style={{width: '100%', height: 500}} // FIXME: unable to get height 100% to work
         mapStyle={'https://demotiles.maplibre.org/style.json'}
       >
         <DeckGLOverlay layers={[layer]} />
