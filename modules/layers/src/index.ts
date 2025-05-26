dcacd54723c335f66fd523e832fba5bc2b5d3e50
Ib Green <ib@openai.com>
2025-05-26 09:05:42 -0400

    feat(layers): GlobalGridLayer, GlobalGrid

--- modules/layers/src/index.ts
+++ modules/layers/src/index.ts
@@ -2,11 +2,20 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) vis.gl contributors
 
-export type {TileSourceLayerProps} from './tile-source-layer/tile-source-layer';
-export {TileSourceLayer} from './tile-source-layer/tile-source-layer';
-
 export type {PathOutlineLayerProps} from './path-outline-layer/path-outline-layer';
 export {PathOutlineLayer} from './path-outline-layer/path-outline-layer';
 
 export type {PathMarkerLayerProps} from './path-marker-layer/path-marker-layer';
 export {PathMarkerLayer} from './path-marker-layer/path-marker-layer';
+
+export type {TileSourceLayerProps} from './tile-source-layer/tile-source-layer';
+export {TileSourceLayer} from './tile-source-layer/tile-source-layer';
+
+export {GlobalGridLayer, type GlobalGridLayerProps} from './global-grid-layer/global-grid-layer';
+
+export {type GlobalGrid} from './global-grid-systems/grids/global-grid';
+export {A5Grid} from './global-grid-systems/grids/a5-grid';
+export {H3Grid} from './global-grid-systems/grids/h3-grid';
+export {S2Grid} from './global-grid-systems/grids/s2-grid';
+export {GeohashGrid} from './global-grid-systems/grids/geohash-grid';
+export {QuadkeyGrid} from './global-grid-systems/grids/quadkey-grid';
