5619be85be3de5114dfc6e80e6bc27fa05d935dd
Ib Green <ib@unfolded.ai>
2025-10-30 20:51:56 -0400

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,30 @@
 import {defineConfig} from 'vite';
-import react from '@vitejs/plugin-react';
 
 import {createHash} from 'crypto';
 
-if (typeof globalThis.crypto.hash !== 'function') {
-  const cryptoPolyfill = {
-    ...globalThis.crypto,
-    hash: (alg) => createHash(alg)
-  };
-
-  Object.defineProperty(globalThis, 'crypto', {
-    value: cryptoPolyfill,
-    writable: true, // Allows the property to be overwritten
-    configurable: true // Allows the property to be redefined or deleted
-  });
-}
-
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    {
+      name: 'clear-host-web-crypto',
+      configureServer() {
+        try {
+          delete (globalThis as any).crypto;
+          if (typeof globalThis.crypto.hash !== 'function') {
+            const cryptoPolyfill = {
+              ...globalThis.crypto,
+              hash: (alg) => createHash(alg)
+            };
+
+            Object.defineProperty(globalThis, 'crypto', {
+              value: cryptoPolyfill,
+              writable: true, // Allows the property to be overwritten
+              configurable: true // Allows the property to be redefined or deleted
+            });
+          }
+        } catch {}
+      }
+    }
+  ],
   resolve: {
     alias: {
       // make Vite itself use Node's builtin crypto in the server process
@@ -26,7 +33,7 @@ export default defineConfig({
   },
   // completely skip the deps optimizer (this is where your stack blows up)
   optimizeDeps: {
-    disabled: true       // <- hard off (works in Vite 5)
+    disabled: true // <- hard off (works in Vite 5)
     // OR: noDiscovery: true  // softer off; if disabled didn't exist in your Vite, try this
   }
 });
