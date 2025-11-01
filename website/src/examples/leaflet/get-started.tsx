e3e0b8c1ff96b2bdff68f05ea4ab4a52d42350a1
Ib Green <ib@unfolded.ai>
2025-11-01 14:57:00 -0400

    fixes

--- website/src/examples/leaflet/get-started.tsx
+++ website/src/examples/leaflet/get-started.tsx
@@ -1,24 +1,37 @@
 import React, {Component} from 'react';
-// import BrowserOnly from '@docusaurus/BrowserOnly';
+import BrowserOnly from '@docusaurus/BrowserOnly';
+
 import {GITHUB_TREE} from '../../constants/defaults';
 import {makeExample} from '../../components';
 
-let initialized = false;
-
 class AdvancedDemo extends Component {
   static title = 'Leaflet as deck.gl Basemap';
+
   static code = `${GITHUB_TREE}/examples/leaflet/get-started`;
 
   static renderInfo(meta) {
     return <></>;
   }
 
+  componentDidMount(): void {
+    import('../../../../examples/leaflet/get-started/app')
+      .then(({exampleApplication}) => exampleApplication())
+      .catch((error) => {
+        console.error('Failed to load Leaflet example', error);
+      });
+  }
+
   render() {
     const {...otherProps} = this.props;
+
     return (
-      <div style={{position: 'absolute', width: '100%', height: '100%', backgroundColor: 'rgba(1,0,0,1)'}}>
-        <div id="map" style={{width: '100%', height: '100%'}} />
-      </div>
+      <BrowserOnly>
+        {() => (
+          <div style={{position: 'absolute', width: '100%', height: '100%', backgroundColor: 'rgba(1,0,0,1)'}}>
+            <div id="map" style={{width: '100%', height: '100%'}} {...otherProps} />
+          </div>
+        )}
+      </BrowserOnly>
     );
   }
 }
