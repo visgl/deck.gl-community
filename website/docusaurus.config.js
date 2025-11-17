a9be742ba2b49c7855169ea7b506996e4d908a07
Ib Green <ib@unfolded.ai>
2025-11-17 10:10:09 -0500

    chore(react): Remove react module

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -88,7 +88,6 @@ const config = {
             '@deck.gl-community/leaflet': resolve('../modules/leaflet/src'),
             '@deck.gl-community/graph-layers': resolve('../modules/graph-layers/src'),
             '@deck.gl-community/infovis-layers': resolve('../modules/infovis-layers/src'),
-            '@deck.gl-community/react': resolve('../modules/react/src'),
             '@deck.gl-community/layers': resolve('../modules/layers/src'),
             '@deck.gl-community/arrow-layers': resolve('../modules/arrow-layers/src'),
             '@deck.gl-community/editable-layers': resolve('../modules/editable-layers/src'),
