010e6feef90a933c4d8b01a9328e372618727083
Charles Richardson <charlesrichardsonusa@gmail.com>
2026-02-17 19:25:48 -0500

    Delete vitest-example directory

--- vitest-example/HelloWorld.test.tsx
+++
@@ -1,8 +0,0 @@
-import { expect, test } from 'vitest'
-import { render } from 'vitest-browser-react'
-import HelloWorld from './HelloWorld.tsx'
-
-test('renders name', async () => {
-  const { getByText } = await render(<HelloWorld name="Vitest" />)
-  await expect.element(getByText('Hello Vitest!')).toBeInTheDocument()
-})
