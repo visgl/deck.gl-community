a452ec791bd8254f339a9eb37c545d7ca853f074
Ib Green <ib@openai.com>
2026-09-02 11:45:40 -0400

    [widgets] add async OmniBox search lifecycle

--- modules/widgets/src/index.ts
+++ modules/widgets/src/index.ts
@@ -32,8 +32,10 @@ export {
   OmniBoxWidget,
   type OmniBoxOption,
   type OmniBoxOptionProvider,
+  type OmniBoxOptionSorter,
   type OmniBoxRenderOptionArgs,
   type OmniBoxResultsSummaryArgs,
+  type OmniBoxResultsState,
   type OmniBoxWidgetProps
 } from './widgets/omni-box-widget';
 export {
