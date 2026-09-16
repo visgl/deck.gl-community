76ec5352cabf59e5d3b4d188abbe4cb486761bf5
Ib Green <ib@unfolded.ai>
2026-09-16 17:14:48 -0400

    fix(playground): address review feedback

--- website/src/docs-sidebar.js
+++ website/src/docs-sidebar.js
@@ -29,6 +29,7 @@ const reactDocs = require('../../docs/modules/react/sidebar.json');
 const experimentalDocs = require('../../docs/modules/experimental/sidebar.json');
 const panelsDocs = require('../../docs/modules/panels/sidebar.json');
 const widgetsDocs = require('../../docs/modules/widgets/sidebar.json');
+const playgroundDocs = require('../../docs/modules/playground/sidebar.json');
 
 const sidebars = {
   tutorialSidebar: [
@@ -71,7 +72,7 @@ const sidebars = {
         title: 'Components',
         description: 'deck.gl-community component packages and integrations.'
       },
-      items: [widgetsDocs, panelsDocs, reactDocs, experimentalDocs]
+      items: [widgetsDocs, panelsDocs, reactDocs, experimentalDocs, playgroundDocs]
     }
   ]
 };
